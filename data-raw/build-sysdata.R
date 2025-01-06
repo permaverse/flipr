efab186352fdd6710357476fbd55184d63e20916
Manon Simonot <manon.simonot20@gmail.com>
2025-01-06 13:03:37 +0100

    Benchmark computation time with parallelization.

--- data-raw/build-sysdata.R
+++ data-raw/build-sysdata.R
@@ -6,11 +6,13 @@ df_mean <- readRDS("data-raw/df_mean.rds")
 df_sd <- readRDS("data-raw/df_sd.rds")
 df_fisher <- readRDS("data-raw/df_fisher.rds")
 df_tippett <- readRDS("data-raw/df_tippett.rds")
+df_parallelization <- readRDS("data-raw/df_parallelization.rds")
 usethis::use_data(
   alpha_estimates,
   pfa, pfb, pfc,
   df_mean, df_sd,
   df_fisher, df_tippett,
+  df_parallelization,
   overwrite = TRUE,
   internal = TRUE
 )
